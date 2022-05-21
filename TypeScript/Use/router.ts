1dc5a56770aa43f8a5ff9272ef3db5e691a64480
容志和 <782494187@qq.com>
2022-05-21 23:56:28 +0800

    front-router study

---
+++ TypeScript/Use/router.ts
@@ -0,0 +1,35 @@
+const router: any = [
+  {
+    path: '/',
+    component: 'Home'
+  },
+  {
+    path: '/about',
+    component: 'About',
+  },
+]
+
+// {
+//   './':{component:'Home'},
+//   './about':{component:'About'}
+// }
+interface routerPath {
+  path: string,
+  component: string
+}
+
+function tranform(router: Array<routerPath>):Object {
+  const res:Object = {}
+  router.forEach((item, index) => {
+    // console.log(res[item.path]);
+    // let pathName: String = item.path
+    // if(pathName && res[pathName as string]) {
+    //   console.log(1);
+    // } 
+    res[item.path] = res[item.path] ? res[item.path] : ""
+    console.log(res);
+  })
+
+  return {}
+}
+tranform(router)
